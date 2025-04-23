0fb8c6e953aa5d974e49b6931d6f77f265bdcfa6
Pablo <Pablogogo.28@hotmail.com>
2025-04-22 20:58:25 -0500

    fix

---
+++ ejercicio2.py
@@ -0,0 +1,16 @@
+print("ingresa edad,nombre, y notas de tu unniversidad")
+name1=input("nombre")
+age=input("edad")
+note1=input("nota 1")
+note2=input("nota 2")
+note3=input("nota 3")
+note4=input("nota 4")
+note5=input("nota 5")
+average=(nota1+nota2+nota3+nota4+nota5)/5
+if average>3: 
+    print("ganaste")
+else:
+    print("perdio la materia")
+
+
+ 
---
+++ prueba.py
@@ -0,0 +1,10 @@
+print("ingresa primera edad")
+edad1 = input()
+print("ingresa segunda edad")
+edad2 = input()
+if edad1 > edad2:
+    print("la edad 1 es mayor")
+else:
+    print("la segunda edad es la mayor")
+
+
